9c4b9f1ed9b5d6e8f9c7ffd4d9bdc4710dc401ff
Abhradip Seth <abhradipseth@Abhradips-MacBook-Air.local>
2025-08-21 21:23:12 +0530

    Updated Feature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <p>Copyright @ 2025,CryptoPlace-By Abhradip Seth-All Right Reserved</p>
-      <p>Created and Maintained by Abhradip Seth</p>
+     
     </div>
   )
 }
